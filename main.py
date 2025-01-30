40ece3f3c3595cac9d2b47a94f7afbabc80f9bd5
CNELYSIA <cn.0535.zrz@outlook.com>
2025-01-30 21:28:38 +0800

    跨域问题处理

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
 import ee
+from starlette.middleware.cors import CORSMiddleware
+
 from utils.ee_downloader import eeDownloader
 ee.Initialize()
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+# noinspection PyTypeChecker
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
